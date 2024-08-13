89cbf0c55629a0cc5d50b12684616cb879eb4967
Lyjor Barros <lyjoramorim@MacBook-M1-Lyjor.local>
2024-08-13 16:31:11 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World - Curso Porto');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
