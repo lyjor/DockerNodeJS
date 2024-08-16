0f9a0745968774c58abf1840b8a5edcd0c69bb4f
Lyjor Barros <lyjoramorim@MacBook-M1-Lyjor.local>
2024-08-16 17:17:41 -0300

    network and compose

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-// app.js
+require('dotenv').config();
+const os = require('os');
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = 8080;
 
+// Configurar a conexão ao banco de dados
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Conectar ao banco de dados
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conectado ao banco de dados MySQL');
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World - Curso Porto NodeJS');
+  const containerName = os.hostname();
+  const dbName = process.env.DB_NAME;
+  result = db.query('select user from user', (err, rows) => {
+    if (err) {
+      console.error('Erro ao executar a consulta:', err);
+      return;
+    }
+    console.log('Consulta retornou:', rows);
+  });
+  print(result);
+  res.send(`Hello World - Curso Porto NodeJS.   -   Nome do contêiner: ${containerName} - DB Name: ${dbName}`);
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Aplicação rodando na porta ${port}`);
 });
